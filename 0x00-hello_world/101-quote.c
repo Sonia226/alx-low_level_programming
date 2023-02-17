fa695e233b94d0ab378df75eed43853e67a211b2
Sonia226 <tsekpodoesoniaselikem@gmail.com>
2023-02-16 21:54:28 -0800

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ * Return 1 (success)
+ */
+
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);        return (1);
+}
