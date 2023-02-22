73a85e2e2e43d9c898919173ca6c2ddbb546c6fb
Sonia226 <tsekpodoesoniaselikem@gmail.com>
2023-02-21 16:21:37 -0800

    1-alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+
+/**
+ * main - check the code
+ *
+ * Return: void
+ */
+
+void print_alphabet(void)
+
+{
+	char c;
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
+	_putchar(c);
+	}
+	_putchar('\n');
+
+}
