350f2bcc92d519342cfab13974a69ff9c048bea4
Sonia226 <tsekpodoesoniaselikem@gmail.com>
2023-02-23 09:09:44 -0800

    2-mul.c

--- 0x04-more_functions_nested_loops/2-mul.c
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -5,12 +5,12 @@
  * mul - Multiplies two integers
  * @x: The first number to multiply
  * @y: The second number to multiply with first number
- * Return: The result of multiplication between x param
+ * Return: The result of multiplication between x param and y param
  */
 
-int mul(int X, int y)
+int mul(int x, int y)
 
 {
-	return(x * y)
+	return (x * y);
 
 }
---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - Print the number since 0 up to 9
+ * Return: The number since 0 up to 9
+ */
+
+void print_numbers(void)
+
+{
+
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+	_putchar(x + '0');
+	}
+	_putchar('\n');
+
+}
