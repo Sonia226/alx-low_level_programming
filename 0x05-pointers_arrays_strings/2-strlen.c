3e31af2604758e0162041fb9701b74b0ee0fdda2
Sonia226 <tsekpodoesoniaselikem@gmail.com>
2023-02-27 17:46:43 -0800

    2-strlen

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -6,12 +6,12 @@
  * Return: The lenght of @str.
  */
 
-size_t_strlen(const char *str)
+size_t _strlen(const char *str)
 
 {
 	size_t lenght = 0;
 
 	while (*str++)
 	lenght++;
-	return (length);
+	return (lenght);
 }
