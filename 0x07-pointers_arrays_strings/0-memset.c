8a8ca138cc0b42ad4489537b66c2eb92a535e799
Sonia226 <tsekpodoesoniaselikem@gmail.com>
2023-03-05 21:15:23 -0800

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -5,7 +5,7 @@
  * @s: pointed destination
  * @b: constant byte
  * @n: bytes
- * Return: Always 0 (success)
+ * Return: Always 0 (Success)
  */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -13,7 +13,7 @@ char *_memset(char *s, char b, unsigned int n)
 {
 	unsigned int i;
 
-	for (i = 0; i < n; i++)
-	       s[i] = b;
+	for (i = 0; i < n; i++
+	s[i] = b;
 	return (s);
 }
