2694bed7c3a6422925e95e3fdf3128ee43630e16
Tsekpodoe Sonia Selikem <tsekpodoesoniaselikem.com>
2023-04-04 10:45:10 -0700

    4-free_listint.c

--- 0x13-more_singly_linked_lists/4-free_listint.c
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -1,13 +1,11 @@
 #include "lists.h"
-
 /**
  * free_listint - frees a linked list
  * @head: listint_t list to be freed
  */
 void free_listint(listint_t *head)
 {
-istint_t *temp;
-
+listint_t *temp;
 while (head)
 {
 temp = head->next;
