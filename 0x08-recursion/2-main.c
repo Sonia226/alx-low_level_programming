e66c3ff86e2a44cf7fa935c3982797786a702e76
Sonia226 <tsekpodoesoniaselikem@gmail.com>
2023-06-07 20:16:26 -0700

    strlen

---
+++ 0x08-recursion/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+    n = strlen_recursion("Corbin Coleman");
+    printf("%d\n", n);
+    return (0);
+}
