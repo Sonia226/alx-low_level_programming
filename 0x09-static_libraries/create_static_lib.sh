4710efd17b93ba3b25faa8beac84d842c6661a35
Sonia226 <tsekpodoesoniaselikem@gmail.com>
2023-06-10 00:16:28 -0700

    libmy liball

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 1. Compile all .c files into object files
- gcc-Wall-Werror-Wextra-pedanric -c *.c
+   gcc -Wall -Werror -Wextra -pedantic -c *.c
 
 2. create the static library from the object files
-    ar -rc liball.a*.c
+    ar -rc liball.a *.o
 
-3. index the static library
+3. indexing
    ranlib liball.a
 
 4.  clean up by removing all the object files
