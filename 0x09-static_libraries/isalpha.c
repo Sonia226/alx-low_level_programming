acac77b41596858b2fb088d4449fa82d9dabba53
Sonia226 <tsekpodoesoniaselikem@gmail.com>
2023-06-09 23:01:02 -0700

    libmy

--- 0x09-static_libraries/isalpha.c
+++ 0x09-static_libraries/isalpha.c
@@ -6,6 +6,5 @@
  */
 int _isalpha(int c)
 {
-
-	return (0);
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
 }
--- 0x09-static_libraries/islower.c
+++ 0x09-static_libraries/islower.c
@@ -6,6 +6,5 @@
  */
 int _islower(int c)
 {
-
-	return (0);
+	return (c >= 'a' && c <= 'z');
 }
