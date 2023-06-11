c640c0e66b54c66d573614bb49b181744ded7a6e
Sonia226 <tsekpodoesoniaselikem@gmail.com>
2023-06-11 07:10:52 -0700

    create_static_lib.sh

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-1. Compile all .c files into object files
-   gcc -Wall -Werror -Wextra -pedantic -c *.c
-
-2. create the static library from the object files
-    ar -rc liball.a *.o
-
-3. indexing
-   ranlib liball.a
-
-4.  clean up by removing all the object files
-    rm *.o
+gcc -Wall -pedantic -Werror -Wextra -c *.c
+ar -rc liball.a *.o
+ranlib liball.a
