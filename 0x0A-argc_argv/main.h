9d5e268216ac1fa5c765479e13c71bfab39e5ca4
Sonia226 <tsekpodoesoniaselikem@gmail.com>
2023-06-11 14:13:13 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,9 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+int _putchar(char c);
+int add_positive_numbers(int argc, char *argv[]);
+int multiply(int num1, int num2);
+int calculateCoins(int cents);
+
+#endif
